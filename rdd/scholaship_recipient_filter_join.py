2c21325e36e399700c60318719f78eb2b4f46883
Vineeth7420 <vineeth.chimbili@gmail.com>
2022-10-21 23:57:28 +0100

    edits

--- rdd/scholaship_recipient_filter_join.py
+++ rdd/scholaship_recipient_filter_join.py
@@ -2,6 +2,64 @@ from pyspark.sql import SparkSession, Row
 from distutils.util import strtobool
 import os.path
 import yaml
+
+if __name__ == '__main__':
+
+    os.environ[] = ()
+
+    spark = SparkSession \
+        .builder \
+        .appName() \
+        .master() \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    app_config_path = os.path.abspath(current_dir + '/../' + 'application.yml')
+    secrets_config_path = os.path.abspath(current_dir + '/../' + '.secrets')
+
+    conf = open(app_config_path)
+    app_conf = yaml.load(conf, Loader=yaml.FullLoader)
+    secret = open(secrets_config_path)
+    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
+    hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoop_conf.set('f3.s3a.access.key', app_secret['s3_conf']['access_key'])
+    hadoop_conf.set('f3.s3a.secret.key', app_secret['s3_conf']['secret_access_key'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from pyspark.sql import SparkSession, Row
+from distutils.util import strtobool
+import os.path
+import yaml
 import sys
 
 if __name__ == '__main__':
