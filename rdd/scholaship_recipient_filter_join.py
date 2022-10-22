4ccb325be13ce97d99d46660c742094e9b563455
Vineeth7420 <vineeth.chimbili@gmail.com>
2022-10-22 01:45:39 +0100

    edits

--- rdd/scholaship_recipient_filter_join.py
+++ rdd/scholaship_recipient_filter_join.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     join_pair_rdd = demographics_pair_rdd \
         .join(finances_pair_rdd)
 
-   
+
 
     courses_rdd = spark.sparkContext.textFile('s3a://' + app_conf['s3_conf']['s3_bucket'] + '/course.csv')
     courses_rdd.foreach(print)
@@ -56,7 +56,8 @@ if __name__ == '__main__':
 
     join_pair_rdd = join_pair_rdd \
         .map(lambda rec: (rec[1][0][6], (rec[0], rec[1][0], rec[1][1]))) \
-        .join(courses_pair_rdd)
+        .join(courses_pair_rdd) \
+        .map(lambda rec: (rec[1][0][0], (rec[1][0][1], rec[1][0][2], rec[1][1])))
 
     join_pair_rdd.foreach(print)
 
